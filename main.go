4bdbebeea70a38f7b616e115c77a60a291eb6be5
Saufi <msaufi2325@gmail.com>
2024-01-24 23:17:57 +0900

    Add function to change string value using a pointer

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "log"
 
+func main() {
+	myString := "Green"
+
+	log.Println("myString is set to ", myString)
+	changeUsingPointer(&myString)
+	log.Println("after func call myString is set to ", myString)
+}
+
+func changeUsingPointer(s *string) {
+	newValue := "Red"
+	*s = newValue
 }
